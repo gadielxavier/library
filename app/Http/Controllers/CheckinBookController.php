aba6ec70d73be533fb2669813e690a4ba0d15658
gadielxavier <gadiel.xavier@gmail.com>
2021-10-08 13:35:27 -0300

    make auth and checkout feature test

---
+++ app/Http/Controllers/CheckinBookController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use App\Book;
+use Illuminate\Http\Request;
+
+class CheckinBookController extends Controller
+{
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+    public function store(Book $book)
+    {
+    	try{
+
+    		$book->checkin(Auth::user());
+
+    	} catch (\Exception $e) {
+
+    		return response([], 404);
+
+    	}
+    }
+}
